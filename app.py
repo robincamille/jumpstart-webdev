c887c9dd5ccaa4ce703f708dd88390de59c38523
Robin Camille Davis <rcdavis6@ncsu.edu>
2021-07-16 14:36:59 -0400

    New routes and templates

--- app.py
+++ app.py
@@ -14,5 +14,16 @@ def new_talk():
     title = talk_mashup_bot.generateTitle()
     return render_template('new_talk.html', title=title)
 
+@app.route('/talk', methods=['POST', 'GET'])
+def talk():
+    if request.method == 'GET':
+        return redirect(url_for('new_talk'))
+
+    title = request.form['title']
+    description = request.form['description']
+
+    return render_template('talk.html', title=title, description=description)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
